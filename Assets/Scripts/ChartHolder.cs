80f81080b2fd5cf012415a0ffaf79063f91fdc58
dwow213 <judahmanoscaleal@gmail.com>
2025-10-28 15:09:06 -0400

    chart now works, printing out the note in the console in sync with music

--- Assets/Scripts/ChartHolder.cs
+++ Assets/Scripts/ChartHolder.cs
@@ -16,11 +16,58 @@ public class ChartHolder : MonoBehaviour
         
     }
 
-    List<Note> GetTestChart()
+    //gets the chart
+    //charts are stored in a 2d float array, where each index holds another float list which holds the values that represent a note
+    //in the embedded list, first index is the measure, second index is the beat and the third index is the movement type
+    public List<List<float>> getChart(string chart)
     {
-        List<Note> chart = [
-            new Note 
-            ]
-        return 1;
+        switch (chart)
+        {
+            case "test":
+                return GetTestChart();
+
+            default:
+                print("invalid chart");
+                return null;
+        }
+    }
+    
+    List<List<float>> GetTestChart()
+    {
+        List<List<float>> chart = new List<List<float>>
+        {
+            new List<float> {1, 1, GetMoveId("up")},
+            new List<float> {1, 2, GetMoveId("down")},
+            new List<float> {1, 3, GetMoveId("left")},
+            new List<float> {1, 4, GetMoveId("right")},
+            new List<float> {2, 1, GetMoveId("up")},
+            new List<float> {2, 2, GetMoveId("down")},
+            new List<float> {2, 3, GetMoveId("left")},
+            new List<float> {2, 4, GetMoveId("right")},
+        };
+
+        return chart;
+    }
+
+    //converts the move type from a string to a float so it can be read by conductor
+    float GetMoveId(string moveType)
+    {
+        switch (moveType)
+        {
+            case "up":
+                return 1;
+
+            case "down":
+                return 2;
+
+            case "left":
+                return 3;
+
+            case "right":
+                return 4;
+            
+            default:
+                return -1;
+        }
     }
 }
--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -4,32 +4,53 @@ using System.Collections.Generic;
 
 public class Conductor : MonoBehaviour
 {
-    
-    public float bpm; //bpm of song
-    public float secPerBeat; //how many seconds each beat takes
-    public float songPosition; //current song position in seconds
-    public float songPositionInBeats; //current song position in beats
-    public float dspSongTime; //how many seconds passed since the song started0
-    public List<Note> chart = new List<Note>(); //list that holds the notes with its measures, beat and type
+    [Header("References")]
+    public ChartHolder chartHolder;
     public AudioSource music; //the music
+
+    [Header("Conductor")]
+    public float bpm; //bpm of song
+    public List<List<float>> chart = new List<List<float>>(); //list that holds the notes with its measures, beat and type
+
+    [Header("Conductor Stats")]
+    [SerializeField] float secPerBeat; //how many seconds each beat takes
+    [SerializeField] float songPosition; //current song position in seconds
+    [SerializeField] int totalBeats; //the total amount of beats in a song
+    [SerializeField] int currentMeasure; //current measure of the song
+    [SerializeField] float currentBeat; //current beat of the song
+    [SerializeField] float dspSongTime; //how many seconds passed since the song started
+
+    
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         music = GetComponent<AudioSource>();
+        secPerBeat = 60f / bpm; //get the amount of seconds per beat based on bpm
+        dspSongTime = (float)AudioSettings.dspTime; //get the amount of time since song started
 
-        //get the amount of seconds per beat based on bpm
-        secPerBeat = 60f / bpm;
-
-        //get the amount of time since song started
-        dspSongTime = (float)AudioSettings.dspTime;
-
+        chart = chartHolder.getChart("test"); //get the test chart
         music.Play(); //play the music
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //determine the song position by checking how many seconds have passed since the song started
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+
+        //get the total amount of beats in the song
+        totalBeats = Mathf.FloorToInt(songPosition / secPerBeat);
+
+        //get the current measure and beat of the song
+        currentMeasure = (int)totalBeats / 4 + 1;
+        currentBeat = (totalBeats % 4) + 1;
+
+        if (chart[0][0] == currentMeasure && chart[0][1] == currentBeat)
+        {
+            print(chart[0][0] + ", " + chart[0][1] + ", " + chart[0][2]);
+            chart.Remove(chart[0]);
+        }
     }
 }
